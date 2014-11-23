a38a7ab7ba5007d4eb06dde8c42032693658a833
Ryo SUETSUGU <suetsugu.r@gmail.com>
2014-11-24 08:01:05 +0900

    Added: save streaks

--- GHStreaks/StreaksViewModel.swift
+++ GHStreaks/StreaksViewModel.swift
@@ -1,5 +1,12 @@
 class StreaksViewModel: RVMViewModel {
     dynamic var currentStreaks = 0
+    let KEY_OF_CURRENT_STREAKS = "CURRENT_STREAKS"
+
+    override init () {
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        currentStreaks = userDefaults.integerForKey(KEY_OF_CURRENT_STREAKS) ?? 0
+    }
+
     func setCurrentStreaks(currentStreaks: Int) {
         self.currentStreaks = currentStreaks
     }
@@ -19,6 +26,8 @@ class StreaksViewModel: RVMViewModel {
                 failure(NSException(name: "JSON error", reason: "fail to parse JSON", userInfo: nil))
             } else {
                 self.currentStreaks = dic!.objectForKey("current_streaks") as Int
+                let userDefaults = NSUserDefaults.standardUserDefaults()
+                userDefaults.setInteger(self.currentStreaks, forKey: self.KEY_OF_CURRENT_STREAKS)
                 success()
             }
 
